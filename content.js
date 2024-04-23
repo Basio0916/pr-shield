8dad2f85e37ff8a4fe932b5c54b2bff67cc134e9
Makoto Ishibashi <arsoncase@gmail.com>
2024-04-23 23:32:58 +0900

    ブランチ変更時に1回ずつボタン無効化処理が呼び出されるように修正

--- content.js
+++ content.js
@@ -1,25 +1,36 @@
-// MutationObserverのコールバック関数を定義
-const callback = function(mutationsList, observer) {
-    // 監視対象のクラスを持つ要素がDOMに追加されたときに発火する
-    for(const mutation of mutationsList) {
-        if (mutation.type === "attributes") {
+// 監視するターゲットの設定
+const targetNode = document.body;
+
+// オプションの設定
+const config = { childList: true, subtree: true };
+
+let lastUrl = '';
+
+// MutationObserverのインスタンスを生成し、コールバック関数とオプションを渡す
+const observer = new MutationObserver(function(mutationsList, observer){
+    if(lastUrl === window.location.href){
+        return;
+    }
+    lastUrl = window.location.href;
+    console.log("observer");
+    const childObserver = new MutationObserver(function(mutationsList, observer){
+        console.log("child observer");
+        for(const mutation of mutationsList) {
             if(mutation.target.classList.contains('open')){
                 const prButton = document.getElementsByClassName("hx_create-pr-button")[0];
                 prButton.setAttribute('disabled', true);
                 observer.disconnect();
             }
         }
-    }
-};
+    });
 
-// 監視するターゲットの設定
-const targetNode = document.getElementsByClassName('js-details-container Details js-compare-pr')[0];
+    const targetNode = document.getElementsByClassName('js-details-container Details js-compare-pr')[0];
+    const config = {attributes: true};
 
-// オプションの設定
-const config = { attributes: true };
-
-// MutationObserverのインスタンスを生成し、コールバック関数とオプションを渡す
-const observer = new MutationObserver(callback);
+    childObserver.observe(targetNode, config);
+});
 
 // ターゲットノードとオプションを渡して監視を開始
 observer.observe(targetNode, config);
+
+console.log("observe");
