11896646b40c05750e340d68152b626ef4eb7d34
Makoto Ishibashi <arsoncase@gmail.com>
2024-04-24 18:00:30 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// 監視するターゲットの設定
+console.log("observe");// 監視するターゲットの設定
 const targetNode = document.body;
 
 // オプションの設定
@@ -13,14 +13,17 @@ const observer = new MutationObserver(function(mutationsList, observer){
     }
     lastUrl = window.location.href;
     console.log("observer");
-    const prButton = document.getElementsByClassName("hx_create-pr-button")[0];
+    const prButton = document.getElementsByClassName('hx_create-pr-button')[0];
+
     if(prButton === undefined){
         return;
     }
     prButton.disabled = true;
 
-    const div = document.createElement('div');
-    div.id = 'pr-shield-container'
+    const container = document.createElement('div');
+    container.id = 'pr-shield-container'
+
+    const div1 = document.createElement('div');
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -41,6 +44,11 @@ const observer = new MutationObserver(function(mutationsList, observer){
     const label = document.createElement('label');
     label.htmlFor = 'pr-shield-checkbox'
     label.className = 'pr-shield-label'
+    label.textContent = '確認しました'
+
+    
+    const div2 = document.createElement('div');
+    div2.id = 'pr-shield-label-container';
 
     const text1 = document.createElement('span');
     text1.className = 'pr-shield-span'
@@ -51,9 +59,8 @@ const observer = new MutationObserver(function(mutationsList, observer){
     text2.className = 'pr-shield-span'
     
     const text3 = document.createElement('span');
-    text3.textContent = 'にマージします。'
+    text3.textContent = 'にマージします'
     text3.className = 'pr-shield-span'
-
 
     const base = document.createElement('span');
     base.id = 'pr-shield-base';
@@ -65,20 +72,35 @@ const observer = new MutationObserver(function(mutationsList, observer){
     head.className = 'pr-shield-span'
     head.textContent = document.getElementById('head-ref-selector').getElementsByClassName('css-truncate css-truncate-target')[0].textContent;
 
-    label.appendChild(text1);
-    label.appendChild(head);
-    label.appendChild(text2);
-    label.appendChild(base);
-    label.appendChild(text3);
+    div1.appendChild(text1);
+    div1.appendChild(head);
+    div1.appendChild(text2);
+    div1.appendChild(base);
+    div1.appendChild(text3);
 
-    div.appendChild(checkbox);
-    div.appendChild(label);
+    div2.appendChild(checkbox);
+    div2.appendChild(label);
+
+    container.appendChild(div1);
+    container.appendChild(div2);
 
     const prForm = document.getElementsByClassName('js-previewable-comment-form')[0];
-    prForm.after(div);
+    prForm.after(container);
+
+    const prButtonObserver = new MutationObserver(function(mutationsList, observer){
+        if(checkbox.checked){
+            prButton.disabled = false;
+        }
+        else{
+            // 下記2つを組み合わせないとdisabledにならない
+            prButton.disabled = true;
+            setTimeout(function(){
+                prButton.disabled = true;
+            }, 1);
+        }
+    });
+    prButtonObserver.observe(prButton, {childList: true});
 });
 
 // ターゲットノードとオプションを渡して監視を開始
 observer.observe(targetNode, config);
-
-console.log("observe");
