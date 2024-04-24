557041e546cfd01aa5416b95d1a47e9d1e90bbe0
Makoto Ishibashi <arsoncase@gmail.com>
2024-04-24 21:12:21 +0900

    マージ時にも表示する対応

--- content.js
+++ content.js
@@ -1,25 +1,119 @@
-console.log("observe");// 監視するターゲットの設定
-const targetNode = document.body;
+let lastPrUrl = '';
 
-// オプションの設定
-const config = { childList: true, subtree: true };
-
-let lastUrl = '';
-
-// MutationObserverのインスタンスを生成し、コールバック関数とオプションを渡す
-const observer = new MutationObserver(function(mutationsList, observer){
-    if(lastUrl === window.location.href){
+const prObserver = new MutationObserver(function(mutationsList, observer){
+    // URLの変更を検出する
+    if(lastPrUrl === window.location.href){
         return;
     }
-    lastUrl = window.location.href;
-    console.log("observer");
-    const prButton = document.getElementsByClassName('hx_create-pr-button')[0];
+    lastPrUrl = window.location.href;
 
+    const prButton = document.getElementsByClassName('hx_create-pr-button')[0];
     if(prButton === undefined){
         return;
     }
+    renderPRConfirmation();
+});
+
+let lastMergeUrl = '';
+const mergeObserver = new MutationObserver(function(mutationsList, observer){
+    // Mergeボタンが現れたときに1度だけ処理をする
+    const mergeButton = document.getElementsByClassName('btn-group-rebase')[0];
+    if(mergeButton === undefined){
+        return;
+    }
+    if(lastMergeUrl === window.location.href){
+        return;
+    }
+    lastMergeUrl = window.location.href;
+    renderMergeConfirmation();
+});
+
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+prObserver.observe(targetNode, config);
+mergeObserver.observe(targetNode, config)
+
+function renderPRConfirmation(){
+    const prButton = document.getElementsByClassName('hx_create-pr-button')[0];
     prButton.disabled = true;
 
+    insertPrConfirmation();
+
+    const prButtonObserver = new MutationObserver(function(mutationsList, observer){
+        observer.disconnect();
+        setPrButtonStatus();
+        observer.observe(prButton, {attributes: true});
+    });
+    prButtonObserver.observe(prButton, {attributes: true});
+
+    const title = document.getElementsByClassName('title')[0];
+    const titleObserver = new MutationObserver(function(mutationsList, observer){
+        setPrButtonStatus();
+    });
+    titleObserver.observe(title, {childList: true})
+}
+
+function renderMergeConfirmation(){
+    const mergeButton = document.getElementsByClassName('btn-group-merge')[0];
+    const squashButton = document.getElementsByClassName('btn-group-squash')[0];
+    const rebaseButton = document.getElementsByClassName('btn-group-rebase')[0];
+
+    if(mergeButton.disabled || squashButton.disabled || rebaseButton.disabled){
+        return;
+    }
+    mergeButton.disabled = true;
+    squashButton.disabled = true;
+    rebaseButton.disabled = true;
+
+    insertMergeConfirmation();
+
+}
+
+function setPrButtonStatus(){
+    const checkbox = document.getElementById('pr-shield-checkbox');
+    const prButton = document.getElementsByClassName('hx_create-pr-button')[0];
+    const title = document.getElementsByClassName('title')[0];
+    if(title.value === ''){
+        prButton.disabled = true;
+        return;
+    }
+    if(checkbox.checked){
+        prButton.disabled = false;
+    }
+    else{
+        // 下記2つを組み合わせないとdisabledにならない
+        prButton.disabled = true;
+        setTimeout(function(){
+            prButton.disabled = true;
+        }, 1);
+    }
+};
+
+function setMergeButtonsStatus(){
+    const checkbox = document.getElementById('pr-shield-checkbox');
+    const mergeButton = document.getElementsByClassName('btn-group-merge')[0];
+    const squashButton = document.getElementsByClassName('btn-group-squash')[0];
+    const rebaseButton = document.getElementsByClassName('btn-group-rebase')[0];    
+
+    if(checkbox.checked){
+        mergeButton.disabled = false;
+        squashButton.disabled = false;
+        rebaseButton.disabled = false;
+    }
+    else{
+        // 下記2つを組み合わせないとdisabledにならない
+        mergeButton.disabled = true;
+        squashButton.disabled = true;
+        rebaseButton.disabled = true;
+        setTimeout(function(){
+            mergeButton.disabled = true;
+            squashButton.disabled = true;
+            rebaseButton.disabled = true;
+        }, 1);
+    }
+}
+
+function insertPrConfirmation(){
     const container = document.createElement('div');
     container.id = 'pr-shield-container'
 
@@ -27,19 +121,7 @@ const observer = new MutationObserver(function(mutationsList, observer){
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.id = 'pr-shield-checkbox'
-    checkbox.addEventListener('change', function(event){
-        if(checkbox.checked){
-            prButton.disabled = false;
-        }
-        else{
-            // 下記2つを組み合わせないとdisabledにならない
-            prButton.disabled = true;
-            setTimeout(function(){
-                prButton.disabled = true;
-            }, 1);
-        }
-    });
+    checkbox.id = 'pr-shield-checkbox';
 
     const label = document.createElement('label');
     label.htmlFor = 'pr-shield-checkbox'
@@ -87,20 +169,65 @@ const observer = new MutationObserver(function(mutationsList, observer){
     const prForm = document.getElementsByClassName('js-previewable-comment-form')[0];
     prForm.after(container);
 
-    const prButtonObserver = new MutationObserver(function(mutationsList, observer){
-        if(checkbox.checked){
-            prButton.disabled = false;
-        }
-        else{
-            // 下記2つを組み合わせないとdisabledにならない
-            prButton.disabled = true;
-            setTimeout(function(){
-                prButton.disabled = true;
-            }, 1);
-        }
-    });
-    prButtonObserver.observe(prButton, {childList: true});
-});
+    checkbox.addEventListener('change', setPrButtonStatus);
+}
 
-// ターゲットノードとオプションを渡して監視を開始
-observer.observe(targetNode, config);
+function insertMergeConfirmation(){
+    const container = document.createElement('div');
+    container.id = 'pr-shield-container'
+
+    const div1 = document.createElement('div');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'pr-shield-checkbox';
+
+    const label = document.createElement('label');
+    label.htmlFor = 'pr-shield-checkbox'
+    label.className = 'pr-shield-label'
+    label.textContent = '確認しました'
+
+    
+    const div2 = document.createElement('div');
+    div2.id = 'pr-shield-label-container';
+
+    const text1 = document.createElement('span');
+    text1.className = 'pr-shield-span'
+    text1.textContent = 'このプルリクエストは';
+
+    const text2 = document.createElement('span');
+    text2.textContent = 'を'
+    text2.className = 'pr-shield-span'
+    
+    const text3 = document.createElement('span');
+    text3.textContent = 'にマージします'
+    text3.className = 'pr-shield-span'
+
+    const base = document.createElement('span');
+    base.id = 'pr-shield-base';
+    base.className = 'pr-shield-span'
+    base.textContent = document.getElementsByClassName('commit-ref')[0].outerText;
+    
+    const head = document.createElement('span');
+    head.id = 'pr-shield-head';
+    head.className = 'pr-shield-span'
+    head.textContent = document.getElementsByClassName('commit-ref')[1].outerText;
+
+    div1.appendChild(text1);
+    div1.appendChild(head);
+    div1.appendChild(text2);
+    div1.appendChild(base);
+    div1.appendChild(text3);
+
+    div2.appendChild(checkbox);
+    div2.appendChild(label);
+
+    container.appendChild(div1);
+    container.appendChild(div2);
+
+    const branchActionItems = document.getElementsByClassName('branch-action-item');
+    const target = branchActionItems[branchActionItems.length - 1];
+    target.after(container);
+
+    checkbox.addEventListener('change', setMergeButtonsStatus);
+}
